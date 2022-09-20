470a4a59e735773e5e7307d56ee09f943e2d795b
pranitashah17 <99394961+pranitashah17@users.noreply.github.com>
2022-09-20 17:16:18 +0530

    Add files via upload

---
+++ supplier.js
@@ -0,0 +1,46 @@
+"use strict"
+import express from "express";
+import {connectionNew} from '../connection.js'
+const router = express.Router();
+  
+
+function supplierList(loc, grdNo, data) {
+    let finalarray = []; 
+    let array = [];
+    for(let i=0; i < data.length; i++) {
+        if(data[i].location === loc) {
+            for(let j=0; j < data[i].listingsData.length; j++) {
+                if(data[i].listingsData[j].gradeNumber === grdNo){
+                array.push({name: data[i].name, price: data[i].listingsData[j].price});
+                }
+            }
+        }      
+    finalarray = array.sort((x, y) => x.price - y.price)
+    }
+    return finalarray
+}
+
+router.get('/', async(req, res) => { 
+    console.log('hello')
+
+    const getData = await connectionNew();
+    const db = getData.db('jsonData')
+    const data = await db        
+        .collection("supplierList")
+        .find({})
+        .toArray()
+  
+
+    let loc = req.query.loc;
+    let grdNo = req.query.grdNo
+     
+    // supplierList(loc, grdNo, data)
+    
+    res.send(supplierList(loc, grdNo, data))
+    // getData.close();
+
+})
+
+// supplierList('5c08cb6f54851e2170a044f4', 'F18S010UA');
+// module.exports = { supplierList }
+export default router;
