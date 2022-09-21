15c8354306f0ae4886e096bda491bb23ca5c12bd
pranitashah17 <99394961+pranitashah17@users.noreply.github.com>
2022-09-21 15:57:19 +0530

    Add files via upload

---
+++ supplier.test.js
@@ -0,0 +1,55 @@
+const { supplierList } = require("./supplier.js");
+
+const fs = require( 'fs');
+let rawdata = fs.readFileSync('jsonData.json');
+let data = JSON.parse(rawdata);
+
+describe("supplier name according to location and grade number", () => {
+    it('supplier list', () => {
+        let loc = '5c08cb6f54851e2170a044f4';
+        let grdNo = 'SRM100NC';
+        let initalizeFunc = supplierList(loc, grdNo, data);
+        expect(initalizeFunc).toStrictEqual(
+            [
+                { name: 'RAHUL (KALPESH PLASTICS)', price: '11650' },
+                { name: 'GAURAV SACHDEVA', price: '11700' },
+                { name: 'ABHISHEK SINDHU', price: '11750' }
+            ]
+          )
+    })
+})
+
+describe("supplier name according to location and grade number", () => {
+    it('supplier list', () => {
+        let loc = '5c77bdb9222ac21701102d22';
+        let grdNo = '582E';
+        let initalizeFunc = supplierList(loc, grdNo, data);
+        expect(initalizeFunc).toStrictEqual(
+            [ { name: 'MILAN SHAH (ANAND TRADELINK)', price: '11900' } ]
+          )
+    })
+})
+
+describe("supplier name according to location and grade number", () => {
+    it('supplier list', () => {
+        let loc = '5d50f89f4546871a15b7e461';
+        let grdNo = 'H110MA';
+        let initalizeFunc = supplierList(loc, grdNo, data);
+        expect(initalizeFunc).toStrictEqual(
+            [ { name: 'SURYA PRAKASH', price: '10750' } ]
+          )
+    })
+})
+
+
+
+
+
+
+// import { parse } from 'node-html-parser';
+
+        // let loc = '5c08cb6f54851e2170a044f4';
+        // const parse = require('node-html-parser');
+        // let grdNo = 'SRM100NC';
+        // console.log(supplierList(loc, grdNo, data))
+        // console.log(data)
