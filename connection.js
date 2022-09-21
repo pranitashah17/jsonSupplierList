c2a6219bf9816e53a2feb829b02e04dfbbb44475
pranitashah17 <99394961+pranitashah17@users.noreply.github.com>
2022-09-21 17:53:15 +0530

    Delete connection.js

--- connection.js
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from "mongodb";
-
-export async function connectionNew () {
-const uri = "mongodb+srv://pranitashah17:PranitaShah17@cluster0.yipyyvq.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-
-await client.connect();
-return client;
-}
